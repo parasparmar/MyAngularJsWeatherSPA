d9f7cc8478b552aa6f0c2d9c2274718ee1808c13
Paras Parmar <paras9may@hotmail.com>
2020-07-30 00:11:57 +0530

    Updated

--- AngularJSWorkAlong/app.js
+++ AngularJSWorkAlong/app.js
@@ -48,10 +48,22 @@ weatherApp.controller('forecastController', ['$scope', '$resource', 'cityService
         units: $scope.units,
         appid: $scope.apiKey
     });
-
+    // Convert F to C.
     $scope.convertToFahrenheit = function (degK) {
         return Math.round((1.8 * (degK - 273)) + 32);
-    };
+    }
+    $scope.convertToCelsius = function (degK) {
+        // debugger;
+        return Math.round(degK - 273.15);
+    }
+    $scope.getWeatherIconUrl = function (iconcode) {
+        
+        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+        return iconurl;
+    }
 
-    //console.log($scope.weatherResult);
+    $scope.convertToDate = function (dt) {
+        return new Date(dt * 1000);
+    }
+    console.log($scope.weatherResult);
 }]);
