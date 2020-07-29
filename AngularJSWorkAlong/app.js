fe2fffdad150ff49e25348fa60c3443eae3e0386
Paras Parmar <paras9may@hotmail.com>
2020-07-29 12:19:14 +0530

    Add project files.

---
+++ AngularJSWorkAlong/app.js
@@ -0,0 +1,33 @@
+﻿// Module
+var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+// ROUTES
+weatherApp.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'pages/home.html',
+            controller: 'homeController'
+        })
+        .when('/forecast', {
+            templateUrl: 'pages/forecast.html',
+            controller: 'forecastController'
+        })
+});
+
+//SERVICES
+weatherApp.service('cityService', function () {
+    this.city = "Mumbai, Maharashtra";
+});
+
+// Controllers
+weatherApp.controller('homeController', ['$scope', 'cityService', function ($scope, cityService) {
+    $scope.city = cityService.city;
+    $scope.$watch('city', function () {
+        cityService.city = $scope.city;
+    });
+}]);
+
+weatherApp.controller('forecastController', ['$scope', 'cityService', function ($scope, cityService) {
+    $scope.city = cityService.city;
+
+}]);
