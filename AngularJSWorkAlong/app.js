0685ae31125b279c57d816e08acbe5d9299aa88e
Paras Parmar <paras9may@hotmail.com>
2020-07-29 20:46:53 +0530

    Updates

--- AngularJSWorkAlong/app.js
+++ AngularJSWorkAlong/app.js
@@ -31,19 +31,27 @@ weatherApp.service('cityService', function () {
 weatherApp.controller('homeController', ['$scope', 'cityService', function ($scope, cityService) {
     $scope.city = cityService.city;
     $scope.$watch('city', function () {
-        cityService.city = $scope.city;
+        cityService.city = $scope.city;        
     });
 }]);
 
 weatherApp.controller('forecastController', ['$scope', '$resource', 'cityService', function ($scope, $resource, cityService) {
     $scope.city = cityService.city;
+    $scope.units = cityService.units;
+    $scope.apiKey = cityService.apiKey;
+
     $scope.weatherApi = $resource("http://api.openweathermap.org/data/2.5/forecast",
         { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" } });
+
     $scope.weatherResult = $scope.weatherApi.get({
-        q: $scope.city,        
+        q: $scope.city,
         units: $scope.units,
         appid: $scope.apiKey
     });
 
-    console.log($scope.weatherResult);
+    $scope.convertToFahrenheit = function (degK) {
+        return Math.round((1.8 * (degK - 273)) + 32);
+    };
+
+    //console.log($scope.weatherResult);
 }]);
